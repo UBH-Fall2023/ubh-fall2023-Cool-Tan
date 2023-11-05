a7b7eea6c4fde7dd5e429e0a3ecfe64a49e10f21
“Jimbob12505” <vatsal.12505@gmail.com>
2023-11-04 22:33:53 -0400

    Restructured project. All run through init now

--- WebScrape/main.py
+++ WebScrape/main.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
 def init():
     menu = {
